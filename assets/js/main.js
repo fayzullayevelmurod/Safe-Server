e13037f19f461641744cfdb5ac922b8891336b70
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-07-10 12:33:29 +0500

    menu media in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,21 @@
 let burger_btn = document.querySelector(".burger_btn");
 let menu_close = document.querySelector(".menu_close");
 let menu_bg = document.querySelector(".menu_bg");
-let menu = document.querySelector(".menu");
+let menu_link = document.querySelector(".menu_link");
 
 burger_btn.addEventListener("click", () => {
   menu_bg.classList.add("active");
-  menu.classList.add("active");
+  menu_link.classList.add("active");
 });
 
 menu_close.addEventListener("click", () => {
   menu_bg.classList.remove("active");
-  menu.classList.remove("active");
+  menu_link.classList.remove("active");
 });
 
 menu_bg.addEventListener("click", () => {
   menu_bg.classList.remove("active");
-  menu.classList.remove("active");
+  menu_link.classList.remove("active");
 });
 // burger
 
