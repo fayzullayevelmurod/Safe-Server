af87fe41030c9de619a6f4c311123f9ac34bfe90
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-07-12 17:56:18 +0500

    fonts added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,4 @@
-// burger
-let burger_btn = document.querySelector(".burger_btn");
-let menu_close = document.querySelector(".menu_close");
-let menu_bg = document.querySelector(".menu_bg");
-let header_link = document.querySelector(".header_link");
 
-burger_btn.addEventListener("click", () => {
-  menu_bg.classList.add("active");
-  header_link.classList.add("active");
-});
-
-menu_close.addEventListener("click", () => {
-  menu_bg.classList.remove("active");
-  header_link.classList.remove("active");
-});
-
-menu_bg.addEventListener("click", () => {
-  menu_bg.classList.remove("active");
-  header_link.classList.remove("active");
-});
-
-header_link.addEventListener("click", () => {
-  menu_bg.classList.remove("active");
-  header_link.classList.remove("active");
-});
-// burger
 
 
 var swiper = new Swiper(".servicesSwiper", {
